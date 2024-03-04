7c1529809b6644c1125324f59b01367cab855cc1
Pierre-Emmanuel.Fabrigli <pierre-emmanuel@fabrigli.fr>
2024-03-04 10:06:24 +0100

    Validation du nom et de la description

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -15,4 +15,15 @@ class Beer < ApplicationRecord
   has_many :notes
   has_many :beer_geeks, through: :notes
   accepts_nested_attributes_for :notes, allow_destroy: true
+
+  #validates_presence_of :name
+  validates :name, presence: true,  uniqueness: { case_sensitive: false }
+  validate :description_include_type
+
+
+  def description_include_type
+    unless description.match?(/[B|b]lanche|[B|b]londe|[A|a]mbrée|IPA/)
+      errors.add(:description, I18n.t("beer.errors.description.does_not_include_type"))
+    end
+  end
 end
