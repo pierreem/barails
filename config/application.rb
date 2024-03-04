5c5d4e26ae766a521a3bc2453695452c2de7cf35
Pierre-Emmanuel.Fabrigli <pierre-emmanuel@fabrigli.fr>
2024-03-04 14:38:39 +0100

    p32 Internationalisation: I18n ou est tu ?

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Barails
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = [:fr, :en]
   end
 end
